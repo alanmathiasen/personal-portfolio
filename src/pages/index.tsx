12dfb7bb349c68cebf74bf69cbc1588edb82f900
alanmathiasen <alanemathiasen@gmail.com>
2023-02-07 14:59:01 -0300

    trying to fix initial scale issue small screns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,35 @@
-import Footer from '@/components/Footer';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Footer from '@/components/Footer';
 import About from '../components/About';
 import Contact from '../components/Contact';
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 import Projects from '../components/Projects';
 
-const Home: NextPage = () => {
-  console.log('just for esplint complaining');
-  return (
-    <div>
-      <Head>
-        <title>Alan Mathiasen - Portfolio</title>
-        <meta
-          name="description"
-          content="Portfolio de Alan Mathiasen - fullstack JavaScript developer"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="bg-body text-white font-poppins pb-12">
-        <Navbar />
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Alan Mathiasen - Portfolio</title>
+      <meta
+        name="description"
+        content="Portfolio de Alan Mathiasen - fullstack JavaScript developer"
+      />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="bg-body text-white font-poppins pb-12">
+      <Navbar />
 
-        <Hero />
-        <main className="w-10/12 md:w-9/12 mx-auto ">
-          <About />
-          <Projects />
-          <Contact />
-        </main>
-        <Footer />
-      </div>
+      <Hero />
+      <main className="w-10/12 md:w-9/12 mx-auto ">
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
     </div>
-  );
-};
+  </div>
+);
 
 export default Home;
